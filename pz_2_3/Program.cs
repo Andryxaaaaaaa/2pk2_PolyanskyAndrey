909b6cd59faaee172c307140787165ee112b6e2b
santa5435@mail.ru <santa5435@mail.ru>
2022-02-17 00:02:08 +0500

    пз 4 , из пз 3(все вместе)

--- pz_2_3/Program.cs
+++ pz_2_3/Program.cs
@@ -5,44 +5,60 @@ namespace pz_2_3
     class Subject
     {
         public string title;//Предмет
-        public int duration;//Количество часов(число)
+        public static int duration;//Количество часов(число)
         public string control;//Зачет или экзамен
-
+        static int test = 0;//статический
+        static int exam = 0;//статический
 
         public Subject(string a, int b, string c) //поля класса
         {
             //Конструктор с числовым и строковым типом переменных
             title = a;
-            duration = b;
             control = c;
+            duration = b;
+
+            //Добавил несколько строчек кода для пз 4
+            //если количество часов вдруг не соответствует заданному промежутку
+            Random rnd = new Random();
+            if (duration < 30 || duration > 160)
+            duration = rnd.Next(30, 160);
+
+            //Проверка на зачет или экзамен
+            if (control == "Зачет")
+                test += 1;
+            if (control == "Экзамен")
+                exam += 1;
 
         }
-
+        
+        
         public void GetInfo() //метод,который выводит информацию о предмете
         {
-            Console.WriteLine($"Предмет: {title}\nКоличество часов: {duration}\nЭкзамен/зачет: {control}\n");
+            Console.WriteLine($"Предмет: {title}\nКоличество часов: {duration}\nЭкзамен/зачет: {control}\n");       
         }
 
         static void Main(string[] args)//Создаем 4 объекта с разными значениями
         {
-            Subject Subject = new Subject("ЭВМ", 400 , "Экзамен");//1
 
+            Subject Subject = new Subject("ЭВМ", 80 , "Экзамен");//1
             Subject.GetInfo();
+            
 
-
-            Subject Subject2 = new Subject("Дискретная Математика", 200, "Зачет");//2
-
+            Subject Subject2 = new Subject("Дискретная Математика", 60, "Экзамен");//2
             Subject2.GetInfo();
+            
 
-
-            Subject Subject3 = new Subject("МДК_04.02", 600, "Экзамен");//3
-
+            Subject Subject3 = new Subject("МДК_04.02", 55, "Экзамен");//3
             Subject3.GetInfo();
+            
 
-
-            Subject Subject4 = new Subject("Физра", 140, "Зачет");//4
-
+            Subject Subject4 = new Subject("Физра", 40, "Зачет");//4           
             Subject4.GetInfo();
+            
+            
+
+            Console.WriteLine($"Зачетов :{test}\nЭкзаменов:{exam}")//вывод зачетов-экзаменов
         }
+        
     }
 }
