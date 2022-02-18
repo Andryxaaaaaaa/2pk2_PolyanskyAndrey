f6a1564d08e6d67142b650bd197c45564e46748e
santa5435@mail.ru <santa5435@mail.ru>
2022-02-19 00:22:38 +0500

    ПРАКТИКА 3 4 5

--- pz_2_3/Program.cs
+++ pz_2_3/Program.cs
@@ -28,37 +28,59 @@ namespace pz_2_3
                 test += 1;
             if (control == "Экзамен")
                 exam += 1;
-
         }
-        
-        
-        public void GetInfo() //метод,который выводит информацию о предмете
+
+        public virtual void GetInfo() //метод,который выводит информацию о предмете
         {
-            Console.WriteLine($"Предмет: {title}\nКоличество часов: {duration}\nЭкзамен/зачет: {control}\n");       
+            Console.WriteLine($"Предмет: {title}\nКоличество часов: {duration}\nЭкзамен/зачет: {control}");       
         }
 
         static void Main(string[] args)//Создаем 4 объекта с разными значениями
         {
 
-            Subject Subject = new Subject("ЭВМ", 80 , "Экзамен");//1
-            Subject.GetInfo();
-            
+            AdditionalSubject AdditionalSubject = new AdditionalSubject("ЭВМ", 80, "Экзамен", "Матрицы");//1
+            AdditionalSubject.GetInfo();
 
-            Subject Subject2 = new Subject("Дискретная Математика", 60, "Экзамен");//2
-            Subject2.GetInfo();
-            
 
-            Subject Subject3 = new Subject("МДК_04.02", 55, "Экзамен");//3
-            Subject3.GetInfo();
-            
+            AdditionalSubject AdditionalSubject2 = new AdditionalSubject("Дискретная Математика", 60, "Экзамен", "СДНФ");//2
+            AdditionalSubject2.GetInfo();
 
-            Subject Subject4 = new Subject("Физра", 40, "Зачет");//4           
-            Subject4.GetInfo();
-            
-            
 
-            Console.WriteLine($"Зачетов :{test}\nЭкзаменов:{exam}")//вывод зачетов-экзаменов
+            AdditionalSubject AdditionalSubject3 = new AdditionalSubject("МДК_04.02", 55, "Экзамен", "Вирусы и Антивирусы");//3
+            AdditionalSubject3.GetInfo();
+
+
+            AdditionalSubject AdditionalSubject4 = new AdditionalSubject("Физра", 40, "Зачет", "Лыжи");//4           
+            AdditionalSubject4.GetInfo();
+
+            Console.WriteLine($"\nЗачетов :{test}\nЭкзаменов:{exam}");//вывод зачетов-экзаменов с практики 4
+
+            Console.WriteLine(); // пустая строчка разделяю
         }
-        
     }
+    class AdditionalSubject : Subject
+    {
+        string Theme;
+
+        public string theme // это конструктор тема как требуется в задании
+        {
+            get => Theme;
+            set => Theme = "Отсутствует значение";
+        }
+        public  AdditionalSubject(string a, int b, string c, string t) : base(a,b,c)
+        {
+            title = a;
+            control = c;
+            duration = b;
+            Theme = t;
+        }
+
+        public override void GetInfo() // переопределяю метод
+        {
+            base.GetInfo();
+            Console.WriteLine($"Тема:{Theme}\n"); // вывожу тему
+        }
+        //Сделана практика 3,4,5
+    }
+
 }
